115c5df077d1556eb91afd268f299a5f74b2058b
Patrik Bezi <p4t0r1k@yahoo.com>
2023-05-23 15:42:36 +0200

    Loading buttonData done

--- gui_lexus/gui_ros.py
+++ gui_lexus/gui_ros.py
@@ -1,7 +1,11 @@
 import sys
 import os
 import rclpy
-import rclpy.node as Node
+from rclpy.node import Node
+
+import pyqtgraph as pg
+import pyqtgraph.Qt as qtgqt
+import pyqtgraph.dockarea as darea
 
 import json
 
@@ -15,10 +19,29 @@ class JSONReader:
         with open(filename, "r") as f:
             self.data = json.load(f)
 
+class PlotHandler(Node):
+    def __init__(self):
+        super().__init__('gui_buttons')
+        scriptDir = os.path.dirname(os.path.realpath(__file__))
+        try:
+            buttonJSON = self.get_parameter('buttons').value
+        except:
+            self.declare_parameter('buttons', 'lexus_buttons.json')
+            buttonJSON = self.get_parameter('buttons').value
+        buttonReader = JSONReader(scriptDir + '/ReadFiles/' + buttonJSON)
+        self.buttonData = buttonReader.data['buttons']
+        print(self.buttonData)
+        self.screenButtons = {}
+        self.runningScreens = []
+        pg.setConfigOptions(antialias=True)
+        self.app = qtgqt.QtGui.QApplication([])
+
 def main(args=None):
     rclpy.init(args=args)
-    scriptDir = os.path.dirname(os.path.realpath(__file__))
-    print(scriptDir)
+    # print(scriptDir)
+    ph = PlotHandler()
+    if (sys.flags.interactive != 1) or not hasattr(qtgqt.QtCore, "PYQT_VERSION"):
+        qtgqt.QtGui.QApplication.instance().exec_()
     rclpy.shutdown()
 
 if __name__ == '__main__':
